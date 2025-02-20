96520ed75bc77456f199febc948bd6010cd9447c
Tanish Hupare <huparetanish@gmail.com>
2025-02-19 18:14:35 -0800

    JWT implemented

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,12 @@
 //login user 
 
-import User from '../model/user.model.js'
+import User from '../model/user.model.js';
+import jwt from 'jsonwebtoken';
+
+ const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d' });
+ }
+
 
 export const loginUser = async(req, res) => {
     res.json({message: 'login user'});
@@ -13,7 +19,11 @@ export const signupUser = async(req, res) => {
     try {
         const user = await User.signup(email, password);
 
-        res.status(200).json({email, user});
+        // create a token
+
+        const token = createToken(user._id);
+
+        res.status(200).json({email, token});
     } catch (error) {
         res.status(400).json({error: error.message })
     }
